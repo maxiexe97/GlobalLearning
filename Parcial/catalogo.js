d397fb77a712a6a390a3fcd1f9dca4d4d4e7dee3
Maxi Lopez <maxi9577@gmail.com>
2021-12-07 03:34:46 -0300

    Recuperacion del primer parcial. Correcciones y agregado de redes sociales

--- Parcial/catalogo.js
+++ Parcial/catalogo.js
@@ -1,12 +1,8 @@
 window.addEventListener('DOMContentLoaded',()=>{
     const orderbuttons = document.querySelectorAll("button[data-order]");
-    //acabo de obtener todos los botones
     console.log(orderbuttons);
-    //ahora por cada uno de los botones:
     orderbuttons.forEach(item => {
-        //escucho el click sobre ese boton:
         item.addEventListener('click', (event)=>{
-            //cuando se presione me mostrara cuál es:
             const button= event.currentTarget;
             const container=button.parentNode;
             let order={
@@ -14,11 +10,9 @@ window.addEventListener('DOMContentLoaded',()=>{
                 price: container.querySelector('.price').innerText,
                 id: button.getAttribute('data-order')
             }
-            //console.log(order);
+            console.log(order);
            localStorage.setItem("order", JSON.stringify(order));
-            //Donde estoy->href | donde quiero ir -> .replace:
             url = window.location.href.replace("catalogo.html","order.html");
-            //ahora le digo que vaya a esa ubicacion
             window.location.href=url;
             
         })
